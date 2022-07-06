e1414c95ff0689b2ef7ba601bd6d6ac776899379
cenkayaycenk <cenkayaycenk@gmail.com>
2022-07-05 20:12:28 -0400

    My second Day

---
+++ Day2_Git_2ndDay/c1_Practice.java
@@ -0,0 +1,15 @@
+package Day2_Git_2ndDay;
+
+public class c1_Practice {
+    public static void main(String[] args) {
+
+        int num=10;
+        int num1=20;
+
+        int sum= num+num1;
+        System.out.println(sum);
+
+    }
+
+
+}
